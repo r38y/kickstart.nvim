f4c0c4366130245cf8d932f5bcb5096726deb1d7
Randy Schmidt <r38y@hey.com>
2025-02-14 00:27:14 -0700

    Clean up language servers

--- init.lua
+++ init.lua
@@ -631,17 +631,15 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        ruby_lsp = {
-          filetypes = { 'ruby' },
-        },
-        sorbet = {
-          filetypes = { 'ruby' },
-        },
-        rubocop = {
-          filetypes = { 'ruby' },
-        },
+        ruby_lsp = {},
+        sorbet = {},
+        rubocop = {},
         elixirls = {},
         tailwindcss = {},
+        cssls = {},
+        yamlls = {},
+        jedi_language_server = {},
+
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
